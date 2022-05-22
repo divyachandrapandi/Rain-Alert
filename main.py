377d94089facf9cd52123fcc9c9c82ff1ca0e888
Divya chandrapandi <98633621+divyachandrapandi@users.noreply.github.com>
2022-05-22 12:24:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,10 @@ import requests
 from twilio.rest import Client   # install, client is a class
 
 
-# TODO Send a sms from twilio
-#  get weather report from openweather api
-#  get twilio trail account
-#  to run in  python anywhere, some changes in code
-#  "https://www.udemy.com/course/100-days-of-code/learn/lecture/21326812#questions"
 
-# TODO Environment variables
-#  It is shown using "env" in mac or pythonanywhere and "set" in windows
-#  export OWM_API_KEY=286ae0ceeb0ecf40cf8866783b83c922 and set OWM_API_LEY=286ae0ceeb0ecf40cf8866783b83c922
-
-API_KEY =  "286ae0ceeb0ecf40cf8866783b83c922" #os.environ.get("OWM_API_KEY")
-account_sid = "ACe96dae203aa3443275c0f123e645e3bf"
-auth_token = "65e6e653e6e504de8059a14b58214d70" #os.environ.get("AUTH_TOKEN")
+API_KEY =  os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("sid")
+auth_token = os.environ.get("AUTH_TOKEN")
 MY_LAT = 10.56
 MY_LONG = 78.48
 
@@ -43,8 +34,8 @@ if will_rain:
     message = client.messages \
         .create(
         body="Today Its gonna Rain...Grab an Umbrella",
-        from_='+19036627947',
-        to='+919952783610'
+        from_=from_num,
+        to=to_num
     )
 
     print(message.status)
